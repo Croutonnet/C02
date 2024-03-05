3d4d5938218c2e605db4756821b087c2fadfc0ed
Raphael Pelchat <rapelcha@c1r1p2.42quebec.com>
2024-03-05 16:46:49 -0500

    oui

--- C06/ex00/main.cpp
+++ C06/ex00/main.cpp
@@ -21,9 +21,44 @@ bool checkIfSpecial(string input){
 	return false;
 }
 
+bool	checkIfFloat(string input){
+	if (input.find("f") == string::npos)
+		return false;
+	return true;
+}
+
+bool	checkIfDouble(string input){
+	
+	return true;
+}
+
+bool	checkIfInt(string input){
+	
+	return true;
+}
+
 int main(int argc, char **argv){
 	if (argc > 2 || argc < 2){
 		return (cout << "Bad arguments" << endl, 1);
 	}
-	bool flag = checkIfSpecial(argv[1]);
+	if (checkIfSpecial(argv[1]) == true){
+		// convertSpecial();
+		return (0);
+	}
+	if (checkIfFloat(argv[1]) == true){
+		// convertFloat();
+		return (0);
+	}
+	if (checkIfDouble(argv[1]) == true){
+		// convertDouble();
+		return (0);
+	}
+	if (checkIfInt(argv[1]) == true){
+		// convertInt();
+		return (0);
+	}
+	else{
+		// convertChar();
+		return (0);
+	}
 }
